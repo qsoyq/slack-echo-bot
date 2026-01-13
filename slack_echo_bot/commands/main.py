b4ed585539e0e3d41c74b21296e43e26cb45cdd5
qsoyq <github@19940731.xyz>
2026-01-13 11:42:44 +0800

    feat: add echo-bot command

--- slack_echo_bot/commands/main.py
+++ slack_echo_bot/commands/main.py
@@ -1,4 +1,8 @@
+
+
 import typer
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from slack_echo_bot.commands import default_invoke_without_command
 
@@ -15,6 +19,41 @@ def add_default_invoke():
 
 
 add_default_invoke()
+
+@cmd.command('echo-bot')
+def echo_bot(slack_bot_token: str = typer.Option(..., envvar='SLACK_BOT_TOKEN'), slack_app_token: str = typer.Option(..., envvar='SLACK_APP_TOKEN')):
+    app = App(token=slack_bot_token)
+    app.command('/echo')(handle_echo_command)
+
+    handler = SocketModeHandler(app, slack_app_token)
+    handler.start()
+
+
+def handle_echo_command(ack, body, respond):
+    """
+    用户在 Slack 输入:
+        /echo hello world
+    Slack 发送一个 command 事件到 Socket Mode，
+    这里的 body["text"] 就是 "hello world"
+    """
+    ack()
+
+    user_id = body['user_id']
+    text = body.get('text', '').strip()
+
+    if not text:
+        respond(
+            response_type='ephemeral',   # 只对发命令的人可见
+            text='用法：`/echo 内容`，例如 `/echo hello`'
+        )
+        return
+
+    respond(
+        response_type='in_channel',     # 频道里所有人可见
+        text=f'<@{user_id}> 说：{text}'
+    )
+
+
 
 if __name__ == '__main__':
     cmd()
